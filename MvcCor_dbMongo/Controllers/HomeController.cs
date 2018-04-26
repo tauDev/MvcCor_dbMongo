facc0296c37eed8c87f98e8bed1edc2d965de6c3
Parinya <parinyat0926@gmail.com>
2018-04-26 18:05:02 +0700

    update
    26/4/2018

--- MvcCor_dbMongo/Controllers/HomeController.cs
+++ MvcCor_dbMongo/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using MvcCor_dbMongo.Models;
 
@@ -19,9 +20,19 @@ namespace MvcCor_dbMongo.Controllers
         }
         public IActionResult Index()
         {
-            mongDB = GetMongoDatabase();
-            var result = mongDB.GetCollection<User>("test").Find(FilterDefinition<User>.Empty).ToList();
-            return View(result);
+            try
+            {
+                mongDB = GetMongoDatabase();
+                var collection = mongDB.GetCollection<User>("test");
+                var result = mongDB.GetCollection<User>("test").Find(FilterDefinition<User>.Empty).ToList();
+                return View(result);
+            }
+            catch (Exception)
+            {
+                //throw;
+                return View();
+            }
+           
         }
 
         public IActionResult About()
@@ -41,6 +52,102 @@ namespace MvcCor_dbMongo.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Create()
+        {
+            mongDB = GetMongoDatabase();
+            // var collection = mongDB.GetCollection<User>("test");
+            try
+            {
+                ViewBag.CountRow = mongDB.GetCollection<User>("test").Find(b => true).Count();
+            }
+            catch(Exception)
+            {
+                ViewBag.CountRow = 0;
+            }            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            try
+            {
+                mongDB = GetMongoDatabase();
+              
+                mongDB.GetCollection<User>("test").InsertOne(user);
+            }catch (Exception)
+            {
+               // throw;
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int? id)
+        {
+            mongDB = GetMongoDatabase();
+
+            var user = mongDB.GetCollection<User>("test").DeleteOne<User>(k => k.No == id);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult Delete(User dd)
+        {
+            try
+            {
+                mongDB = GetMongoDatabase();
+                var result = mongDB.GetCollection<User>("test").DeleteOne<User>(k => k.No == dd.No);
+                if (result.IsAcknowledged == false)
+                {
+                    return BadRequest("Unable to Delete User " + dd.No);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details(int? id)
+        {
+            mongDB = GetMongoDatabase();
+
+            var user = mongDB.GetCollection<User>("test").Find<User>(k => k.No == id).FirstOrDefault();
+            return View(user);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+            mongDB = GetMongoDatabase();
+            var users = mongDB.GetCollection<User>("test").Find<User>(a => a.No == id).FirstOrDefault();
+            if(users==null)
+            {
+                return NotFound();
+            }
+           
+            return View(users);
+        }
+        [HttpPost]
+        public IActionResult Edit(User users)
+        {
+            try
+            {
+                mongDB = GetMongoDatabase();
+                var filter = Builders<User>.Filter.Eq("No", users.No);
+                var updatestatement = Builders<User>.Update.Set("No", users.No);
+                updatestatement = updatestatement.Set("Uname", users.Uname);
+                updatestatement = updatestatement.Set("Password", users.Password);
+                var result = mongDB.GetCollection<User>("test").UpdateOne(filter, updatestatement);
+                if(result.IsAcknowledged==false)
+                {
+                    return BadRequest("Unable to update Customer " + users.Uname);
+                }
+            }catch(Exception)
+            {
+
+            }
+            return RedirectToAction("Index");
         }
     }
 }
--- MvcCor_dbMongo/Models/User.cs
+++ MvcCor_dbMongo/Models/User.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcCor_dbMongo.Models
 {
@@ -12,7 +13,11 @@ namespace MvcCor_dbMongo.Models
         [BsonId]
         public ObjectId _id { get; set; }
         [BsonElement]
+        public int No { get; set; }
+        [Required]
+        [BsonElement]
         public string Uname { get; set; }
+        [Required]
         [BsonElement]
         public string Password { get; set; }
     }
